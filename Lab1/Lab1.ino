8c7d214d8872db952a614384d2fdd20fa12c4984
Jakob Snyder <jakobjsnyder@gmail.com>
2017-01-25 13:09:10 -0600

    completing some parts of lab 1

--- Lab1/Lab1.ino
+++ Lab1/Lab1.ino
@@ -1,4 +1,6 @@
 # define DEBUG_OUTPUT_MESSAGE_MAX_LENGTH 80
+
+
 void setup () {
 // put your setup code here , to run once :
 Serial . begin (9600) ;
@@ -13,8 +15,71 @@ snprintf ( message , DEBUG_OUTPUT_MESSAGE_MAX_LENGTH ,
 "It took %d microseconds to generate and send ’Hello World ! ’\n",
 end_time - begin_time ) ;
 Serial . write ( message ) ;
+
+  char testCharArray[80] = "Test Sentence #1";
+  char* ptr = firstName;
+  myToUpper(ptr);
+  printf(firstName);  
+
+
 }
 void loop () {
 // put your main code here , to run repeatedly :
 }
+
+void getVarSizes(){
+  char message [ DEBUG_OUTPUT_MESSAGE_MAX_LENGTH ];
+  char testChar;
+  int testCharSize = 0;
+  short testShort;
+  int testShortSize = 0;
+  int testInt;
+  int testIntSize = 0;
+  long testLong;
+  int testLongSize = 0;
+  long long testLongLong;
+  int testLongLongSize = 0;
+
+  char *addr1, *addr2;
+  addr1 = (char*)&testChar;
+  addr2 = (char*)(&testChar + 1);
+  testCharSize = (int)(addr2-addr1);
+
+  addr1 = (char*)&testShort;
+  addr2 = (char*)(&testShort + 1);
+  testShortSize = (int)(addr2-addr1);
+
+  addr1 = (char*)&testInt;
+  addr2 = (char*)(&testInt + 1);
+  testIntSize = (int)(addr2-addr1);
+
+  addr1 = (char*)&testLong;
+  addr2 = (char*)(&testLong + 1);
+  testLongSize = (int)(addr2-addr1);
+
+  addr1 = (char*)&testLongLong;
+  addr2 = (char*)(&testLongLong + 1);
+  testLongLongSize = (int)(addr2-addr1);
+  
+  snprintf ( message , DEBUG_OUTPUT_MESSAGE_MAX_LENGTH , "Experimental char size = %d size of = %d\n", testCharSize, sizeof(char));
+  snprintf ( message , DEBUG_OUTPUT_MESSAGE_MAX_LENGTH , "Experimental short size = %d size of = %d\n", testShortSize, sizeof(short));
+  snprintf ( message , DEBUG_OUTPUT_MESSAGE_MAX_LENGTH , "Experimental int size = %d size of = %d\n", testIntSize, sizeof(int));
+  snprintf ( message , DEBUG_OUTPUT_MESSAGE_MAX_LENGTH , "Experimental long size = %d size of = %d\n", testLongSize, sizeof(long));
+  snprintf ( message , DEBUG_OUTPUT_MESSAGE_MAX_LENGTH , "Experimental long long size = %d size of = %d\n\n", testLongLongSize, sizeof(long long));
+}
+
+void myToUpper(char* pointer){
+    int i = 0;
+    for(i = 0; i < 80; i++){
+        if(pointer[i] == '\0')
+            break;
+        if((int)pointer[i]>=97 && (int)pointer[i]<=122)
+            pointer[i]-=32;
+    }
+}
+
+void flipSign(int32_t* pointer){
+  
+}
+
 
